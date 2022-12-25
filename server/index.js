8a0b23748409d51db451cc5758b07fcd2e69e4f9
AleksARPV <ascanalex@gmail.com>
2022-12-25 20:28:05 +0300

    prep for docker

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ const path = require('path')
 const PORT = process.env.PORT || 8080
 
 const app = express()
+
 app.use(cors())
 app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'static')))
@@ -20,6 +21,16 @@ app.use('/api', router)
 
 app.use(errorHandler)
 
+
+if (process.env.NODE_ENV === 'production') {
+    app.use('/', express.static(path.join(__dirname, 'client')))
+
+    const indexPath = path.join(__dirname, 'client', 'index.html')
+
+    app.get('*', (req, res) => {
+        res.sendFile(indexPath)
+    })
+}
 
 const start = async () => {
     try {
